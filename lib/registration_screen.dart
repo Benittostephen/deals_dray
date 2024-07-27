18dcbda33e681f7d13edc4619b1d565085ae2e5f
Benittostephen <benittostephen2000@gmail.com>
2024-07-27 10:32:40 +0530

    Add Dependency

--- lib/registration_screen.dart
+++ lib/registration_screen.dart
@@ -95,142 +95,148 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: IconButton(
-                icon:
-                    Image.asset('assets/icons/back.png', height: height * 0.03),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-            Opacity(
-              opacity: 0.5,
-              child: Container(
-                color: Colors.red,
-                height: height * 0.17,
-                width: width * 0.41,
-                child: Image.asset(
-                  'assets/images/dealsdray_logo.jpeg',
-                  fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  icon: Image.asset('assets/icons/back.png',
+                      height: height * 0.03),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ),
-            ),
-            SizedBox(height: height * 0.02),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Let's Begin!",
-                      style: TextStyle(
-                          fontSize: width * 0.08, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: height * 0.01),
-                    Text(
-                      'Please enter your credentials to proceed',
-                      style: TextStyle(
-                          fontSize: width * 0.04, color: Colors.grey.shade600),
-                    ),
-                    SizedBox(height: 70),
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Your Email',
-                        labelStyle:
-                            TextStyle(fontSize: 20, color: primaryColor),
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your email';
-                        }
-                        final regex = RegExp(
-                            r'^[a-zA-Z0-9.a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
-                        if (!regex.hasMatch(value)) {
-                          return 'Please enter a valid email address';
-                        }
-                        return null; // No error
-                      },
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Create Password',
-                        labelStyle:
-                            TextStyle(fontSize: 20, color: primaryColor),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isPasswordVisible
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                            color: Colors.grey,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isPasswordVisible = !_isPasswordVisible;
-                            });
-                          },
-                        ),
-                      ),
-                      obscureText: !_isPasswordVisible,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        if (value.length < 8) {
-                          return 'Password must be at least 8 characters long';
-                        }
-                        return null; // No error
-                      },
-                    ),
-                    SizedBox(height: 20),
-                    TextField(
-                      controller: _referralCodeController,
-                      decoration: InputDecoration(
-                        labelText: 'Referral Code (Optional)',
-                        labelStyle:
-                            TextStyle(fontSize: 20, color: primaryColor),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Spacer(),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-              child: Align(
-                alignment: Alignment.bottomRight,
+              Opacity(
+                opacity: 0.5,
                 child: Container(
-                  height: 70,
-                  width: 80,
-                  child: FloatingActionButton(
-                    onPressed: _isLoading ? null : _register,
-                    backgroundColor: secondaryColor,
-                    child: (_isLoading)
-                        ? CircularProgressIndicator(
-                            valueColor:
-                                AlwaysStoppedAnimation<Color>(Colors.white))
-                        : Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                            size: 30,
-                          ),
+                  color: Colors.red,
+                  height: height * 0.17,
+                  width: width * 0.41,
+                  child: Image.asset(
+                    'assets/images/dealsdray_logo.jpeg',
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: height * 0.02),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 40),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Let's Begin!",
+                        style: TextStyle(
+                            fontSize: width * 0.08,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Text(
+                        'Please enter your credentials to proceed',
+                        style: TextStyle(
+                            fontSize: width * 0.04,
+                            color: Colors.grey.shade600),
+                      ),
+                      SizedBox(height: 70),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Your Email',
+                          labelStyle:
+                              TextStyle(fontSize: 20, color: primaryColor),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          final regex = RegExp(
+                              r'^[a-zA-Z0-9.a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+                          if (!regex.hasMatch(value)) {
+                            return 'Please enter a valid email address';
+                          }
+                          return null; // No error
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Create Password',
+                          labelStyle:
+                              TextStyle(fontSize: 20, color: primaryColor),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isPasswordVisible
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isPasswordVisible = !_isPasswordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        obscureText: !_isPasswordVisible,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          if (value.length < 8) {
+                            return 'Password must be at least 8 characters long';
+                          }
+                          return null; // No error
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: _referralCodeController,
+                        decoration: InputDecoration(
+                          labelText: 'Referral Code (Optional)',
+                          labelStyle:
+                              TextStyle(fontSize: 20, color: primaryColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.18,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 30.0, vertical: 30.0),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    height: 70,
+                    width: 80,
+                    child: FloatingActionButton(
+                      onPressed: _isLoading ? null : _register,
+                      backgroundColor: secondaryColor,
+                      child: (_isLoading)
+                          ? CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white))
+                          : Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
